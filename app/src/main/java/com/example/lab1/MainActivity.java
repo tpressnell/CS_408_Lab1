7fae2be40e800ebd81ce2725a3a4ffee81ed6da3
tyler <tyler.pressnell86@gmail.com>
2021-01-26 11:56:47 -0600

    Initial Commit after restarting project.

---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String counter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        counter = "50";
+        TextView t = (TextView) findViewById(R.id.counterOutput);
+        t.setText(counter);
+    }
+
+    public void onClickIncrementButton(View V){
+        TextView t = (TextView) findViewById(R.id.counterOutput);
+        int intCounter = Integer.parseInt(counter);
+        intCounter++;
+        counter = String.valueOf(intCounter);
+        t.setText(counter);
+    }
+
+    public void onClickDecrementButton(View V){
+        TextView t = (TextView) findViewById(R.id.counterOutput);
+        int intCounter = Integer.parseInt(counter);
+        intCounter--;
+        counter = String.valueOf(intCounter);
+        t.setText(counter);
+    }
+}
